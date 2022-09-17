26c879ed8b892e42603dc9fd222df39e7c646f62
Everson <eversonalba98@hotmail.com>
2022-09-17 10:57:52 -0500

    Actualizar datos

--- src/main/java/pe/isil/moduloseguridad/ModuloseguridadApplication.java
+++ src/main/java/pe/isil/moduloseguridad/ModuloseguridadApplication.java
@@ -8,7 +8,7 @@ public class ModuloseguridadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModuloseguridadApplication.class, args);
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo soy Everson");
 	}
 
 }
